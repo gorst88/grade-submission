e1563c0b4fc8d52601f54ef710f6c2d7dbea408b
Goran <stoykov.g@outlook.de>
2022-11-13 20:21:18 +0100

    feat: form validation

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -3,8 +3,11 @@ package com.ltp.gradesubmission;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +18,16 @@ public class GradeController {
     List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/")
-    public String gradeForm(Model model, @RequestParam(required = false) String id) {
+    public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getGradeIndex(id);
         model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : studentGrades.get(index));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
-    public String submitForm(Grade grade) {
+    public String submitForm(@Valid Grade grade, BindingResult result) {
         int index = getGradeIndex(grade.getId());
+        if (result.hasErrors()) return "form";
         if (index == Constants.NOT_FOUND) {
             studentGrades.add(grade);
         } else {
